018e75f7909772a4a89e627a91f59e312fa644bf
Alex Conway <abconway2@gmail.com>
2023-09-19 17:02:12 -0400

    animate!

--- World/Grass.gd
+++ World/Grass.gd
@@ -4,7 +4,8 @@ extends Node2D
 func _physics_process(delta: float) -> void:
 	if Input.is_action_just_pressed("player_attack"):
 		var grassEffect := load("res://Effects/GrassEffect.tscn")
-		var grass_effect: GrassEffect = grassEffect.instance()
+		var grass_effect: GrassEffect = grassEffect.instantiate()
 		var root := get_tree().current_scene
-		
+		root.add_child(grass_effect)
+		grass_effect.global_position = global_position
 		queue_free()
